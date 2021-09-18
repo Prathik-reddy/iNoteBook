00fb99f1847d3b7e8a9362ac5bdbc451d5c151e2
Prathik Reddy <tvkp333555@gmail.com>
2021-09-18 21:23:21 +0530

    added note item in seperate note component

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -20,6 +20,60 @@ const NoteState = (props) => {
           "tag": "this is a tag 2",
           "date": "2021-09-16T18:04:04.076Z",
           "__v": 0
+        },
+        {
+          "_id": "614387142bdd9086d0c7dcef",
+          "user": "6140a877170ddbcb144f9603",
+          "title": "My title 2",
+          "description": "this is a description 2",
+          "tag": "this is a tag 2",
+          "date": "2021-09-16T18:04:04.076Z",
+          "__v": 0
+        },
+        {
+          "_id": "614387142bdd9086d0c7dcef",
+          "user": "6140a877170ddbcb144f9603",
+          "title": "My title 2",
+          "description": "this is a description 2",
+          "tag": "this is a tag 2",
+          "date": "2021-09-16T18:04:04.076Z",
+          "__v": 0
+        },
+        {
+          "_id": "614387142bdd9086d0c7dcef",
+          "user": "6140a877170ddbcb144f9603",
+          "title": "My title 2",
+          "description": "this is a description 2",
+          "tag": "this is a tag 2",
+          "date": "2021-09-16T18:04:04.076Z",
+          "__v": 0
+        },
+        {
+          "_id": "614387142bdd9086d0c7dcef",
+          "user": "6140a877170ddbcb144f9603",
+          "title": "My title 2",
+          "description": "this is a description 2",
+          "tag": "this is a tag 2",
+          "date": "2021-09-16T18:04:04.076Z",
+          "__v": 0
+        },
+        {
+          "_id": "614387142bdd9086d0c7dcef",
+          "user": "6140a877170ddbcb144f9603",
+          "title": "My title 2",
+          "description": "this is a description 2",
+          "tag": "this is a tag 2",
+          "date": "2021-09-16T18:04:04.076Z",
+          "__v": 0
+        },
+        {
+          "_id": "614387142bdd9086d0c7dcef",
+          "user": "6140a877170ddbcb144f9603",
+          "title": "My title 2",
+          "description": "this is a description 2",
+          "tag": "this is a tag 2",
+          "date": "2021-09-16T18:04:04.076Z",
+          "__v": 0
         }
       ]
       const [notes, setNotes] = useState(notesInitial)
