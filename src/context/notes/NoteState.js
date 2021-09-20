716a5184a2d5dcf9d0afd559e01eb71408ef24d4
Prathik Reddy <tvkp333555@gmail.com>
2021-09-20 20:18:53 +0530

    added update note functionality

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -16,6 +16,8 @@ const NoteState = (props) => {
       },
       body: JSON.stringify({ title, description, tag })
     });
+    const json = await response.json();
+    console.log(json);
     let note = {
       "_id": "614387142bdd9086d6c7dcef",
       "user": "6140a877170ddbcb144f9603",
@@ -41,7 +43,17 @@ const NoteState = (props) => {
     setNotes(json);
   }
   // Delete a note
-  const deleteNote = (id) => {
+  const deleteNote = async (id) => {
+    // api call
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: 'DELETE', headers: {
+        "Content-Type": "application/json",
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjE0MGE4NzcxNzBkZGJjYjE0NGY5NjAzIn0sImlhdCI6MTYzMTcyNjYxNn0.yP8AGxsqlRBI7EAuz7hT2d_N209Hz1DUC0HG0GLLTTA"
+      },
+    });
+    const json = await response.json();
+    console.log(json);
+
     console.log("del note with id" + id);
     const newNotes = notes.filter((note) => { return note._id !== id });
     setNotes(newNotes);
@@ -49,22 +61,26 @@ const NoteState = (props) => {
   // Edit a note
   const editNote = async (id, title, description, tag) => {
     const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
-      method: 'POST', headers: {
+      method: 'PUT', headers: {
         "Content-Type": "application/json",
         "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjE0MGE4NzcxNzBkZGJjYjE0NGY5NjAzIn0sImlhdCI6MTYzMTcyNjYxNn0.yP8AGxsqlRBI7EAuz7hT2d_N209Hz1DUC0HG0GLLTTA"
       },
       body: JSON.stringify({ title, description, tag })
     });
-    const json = response.json();
+    const json = await response.json();
+    console.log(json);
 
-    for (let index = 0; index < notes.length; index++) {
-      const element = notes[index];
+    let newNotes= JSON.parse(JSON.stringify(notes));
+    for (let index = 0; index < newNotes.length; index++) {
+      const element = newNotes[index];
       if (element._id === id) {
-        element.title = title;
-        element.description = description;
-        element.tag = tag;
+        newNotes[index].title = title;
+        newNotes[index].description = description;
+        newNotes[index].tag = tag;
+        break;
       }
     }
+    setNotes(newNotes);
   }
   return (
     <NoteContext.Provider value={{ notes, getNotes,editNote, deleteNote, addNote }} >
